376893e7bb7e3ded82d64a287d7dba5d3588e19d
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2020-10-04 21:44:53 +0530

    started with adding views and models

---
+++ lib/helper/data.dart
@@ -0,0 +1,37 @@
+import 'package:flutterapp/models/category_model.dart';
+
+List<CategoryModel> getCategories() {
+  List<CategoryModel> category = new List<CategoryModel>();
+
+  CategoryModel categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Buisness";
+  categoryModel.imageUrl = "https://images.unsplash"
+      ".com/photo-1465776702172-defce4b4d574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Buisness";
+  categoryModel.imageUrl = "https://images.unsplash"
+      ".com/photo-1465776702172-defce4b4d574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Buisness";
+  categoryModel.imageUrl = "https://images.unsplash"
+      ".com/photo-1465776702172-defce4b4d574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Buisness";
+  categoryModel.imageUrl = "https://images.unsplash"
+      ".com/photo-1465776702172-defce4b4d574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Buisness";
+  categoryModel.imageUrl = "https://images.unsplash"
+      ".com/photo-1465776702172-defce4b4d574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  return category;
+}
